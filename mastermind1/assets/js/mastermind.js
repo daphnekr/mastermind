9023978232f001e6ef122751bfef70355a9322d2
Daphne Kramer <99046027@mydavinci.nl>
2020-01-08 10:50:52 +0100

    Blok 2
    Week 14 tm 17

--- mastermind1/assets/js/mastermind.js
+++ mastermind1/assets/js/mastermind.js
@@ -14,37 +14,70 @@ var colorArray = [
     answer3 = colors[random3],
     answer4 = colors[random4]
 ]
+var text = document.getElementsByTagName("p")[0];
+var checkBtn = document.getElementById("check");
 console.log(colorArray);
 
 
 
 function check(){
-    var btn;
-    for(i=0; i<4; i++){
-        btn = document.getElementById("letter"+row+"-"+i);
-
-        if(colorArray[0] === btn.value){
+    for(column = 0; column < 4; column++){
+        btn = document.getElementById("button"+row+"-"+column);
+        btnValue = btn.value;
+        checks = document.getElementById("check"+row+"-"+column);
+        
+        if(colorArray[column] === btnValue){
             checks.style.height = "30px";
             checks.style.width = "30px";
-            checks.style.background = "black";
+            checks.style.background = "red";
             checks.style.border = "2px solid #000000";
             checks.style.borderRadius = "40px"
-
         }
+
+        else if (colorArray.includes(btnValue)){
+                checks.style.height = "30px";
+                checks.style.width = "30px";
+                checks.style.background = "white";
+                checks.style.border = "2px solid #000000";
+                checks.style.borderRadius = "40px"
+        }
+
     }
-    for(column = 0; column < 4; column++){
-        checks = document.getElementById("check"+row+"-"+column);
-        checks.style.height = "30px";
-        checks.style.width = "30px";
-        checks.style.background = "grey";
-        checks.style.border = "2px solid #000000";
-        checks.style.borderRadius = "40px"
-    }
-    row++;
-    var column = 0;
+    
+
+        var checkColors = 0;
+
+        for (column = 0 ; column < 4; column++){
+            btn = document.getElementById('button'+row+'-'+column);
+            btnValue = btn.value;
+            if (colorArray[column] != btnValue){
+                checkColors = 1;
+            }
+        }
+        
+        if(checkColors == 0){
+            text.innerHTML = "Je hebt het kleurcode geraden! Gefeliciteerd. Speel opnieuw."
+            checkBtn.innerHTML = "Speel opnieuw";
+            checkBtn.setAttribute("onclick", "playAgain()");
+            checkBtn.style.background = "rgb(128, 255, 96)";
+        }
+
+        if(checkColors == 1){
+            row++;
+        }
+        console.log(row);
+        if (row == 12){
+            text.innerHTML = "Je hebt de kleurcode niet geraden. De juiste kleurcode was: "+ colorArray + ". Speel opnieuw."
+            checkBtn.innerHTML = "Speel opnieuw";
+            checkBtn.setAttribute("onclick", "playAgain()");
+            checkBtn.style.background = "rgb(128, 255, 96)";
+        }
+        column = 0;
+        
 }
 var column = 0;
 function next(){
+    console.log(document.getElementById("button"+row+"-"+column).value);
     column++;
 }
 
@@ -52,13 +85,13 @@ function red(){
 
     document.getElementById("button"+row+"-"+column).style.backgroundColor = "red";  
     document.getElementById("button"+row+"-"+column).setAttribute("onclick", "blue()"); 
-    document.getElementById("button"+row+"-"+column).value = "red";
+    document.getElementById("button"+row+"-"+column).setAttribute("value", "red");
 }
 function blue(){
 
     document.getElementById("button"+row+"-"+column).style.backgroundColor = "blue";  
     document.getElementById("button"+row+"-"+column).setAttribute("onclick", "green()"); 
-    document.getElementById("button"+row+"-"+column).value = "blue";
+    document.getElementById("button"+row+"-"+column).setAttribute("value", "blue");
 
 }
 function green(){
@@ -86,4 +119,8 @@ function black(){
     document.getElementById("button"+row+"-"+column).setAttribute("onclick", "red()"); 
     document.getElementById("button"+row+"-"+column).value = "black";
 
+}
+
+function playAgain(){
+    location.reload();
 }
